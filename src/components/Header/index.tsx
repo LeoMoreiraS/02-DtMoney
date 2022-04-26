48c36ee852c26658bd09ed961a2945674860d21c
leonardo.sepulveda <leomoreira.com@gmail.com>
2022-04-26 12:34:00 -0300

    Adicionando Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,23 @@
+
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
 
+interface HeaderProps{
+    
+    onOpenNewTransactionModal: () => void;
 
-export function Header(){
+}
+
+export function Header(props: HeaderProps) {
+   
     return(
         <Container>
             <Content>
            <img src={logoImg} alt = "dt money"/>
-           <button type="button">
+           <button type="button" onClick={props.onOpenNewTransactionModal}>
                Nova transação
            </button>
+           
            </Content>
         </Container>
     )
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 
 createServer({
+  models:{
+    transaction: Model,
+    
+  },
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id:1,
+          title: 'Freela',
+          type: 'deposit',
+          amount: 3000,
+          category: 'Dev',
+          createdAt: new Date('2022-04-26 10:00:00')
+        },
+        {
+          id:2,
+          title: 'Compra',
+          type: 'withdraw',
+          amount: 500,
+          category: 'Casa',
+          createdAt: new Date('2022-04-23 10:00:00')
+        },
+        {
+          id:3,
+          title: 'Salário',
+          type: 'deposit',
+          amount: 1000,
+          category: 'Normal',
+          createdAt: new Date('2022-04-22 10:00:00')
+        },
+      ],
+      
+    })
+  },
   routes(){
     this.namespace = 'api';
     this.get('/transactions',()=>{
       return [
-        {
-          id:1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date(),
-        }
+        this.schema.all('transaction')
       ]
+    })
+    this.post('/transactions', (schema, request)=>{
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
     })
   }
 })
