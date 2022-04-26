3f1a2620b0f6ec829b3884011b223189f59d23c6
leonardo.sepulveda <leomoreira.com@gmail.com>
2022-04-26 13:33:58 -0300

    Finalizando usos de contexto

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, ReactNode, useEffect, useState } from 'react';
 import { api } from './services/api';
 interface Transaction{
-    id:number;
+    id?:number;
     title:string;
     amount:number;
     type:string;
@@ -9,11 +9,18 @@ interface Transaction{
     createdAt:string;
 
 }
+
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
 interface TransactionsProviderProps{
     children: ReactNode;
 }
-
-export const TransactionsContext = createContext<Transaction[]>([]);
+interface TransactionsContextData {
+    transactions: Transaction[];
+    createTransaction: (transction:TransactionInput) => Promise<void>;
+}
+export const TransactionsContext = createContext<TransactionsContextData>(
+    {} as TransactionsContextData
+);
 
 export function TransactionsProvider({children}:TransactionsProviderProps){
     const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -21,8 +28,15 @@ export function TransactionsProvider({children}:TransactionsProviderProps){
         api.get('https://localhost:3000/api/transactions')
             .then(response => setTransactions(response.data.transactions));
     },[])
+
+    async function createTransaction(transactionInput:TransactionInput){
+           const response = await api.post('/transactions', {...transactionInput, createdAt: new Date()});
+           const {transaction} = response.data;
+           setTransactions([...transactions,transaction])
+    }
+
     return (
-        <TransactionsContext.Provider value={transactions}>
+        <TransactionsContext.Provider value={{transactions, createTransaction}}>
             {children}
         </TransactionsContext.Provider>
     )
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,37 +1,66 @@
 import { Container } from "./styles";
-import incomeImg from '../../assets/income.svg'
-import outcomeImg from '../../assets/outcome.svg'
-import totalImg from '../../assets/total.svg'
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import totalImg from "../../assets/total.svg";
 import { TransactionsContext } from "../../TransactionsContext";
 import { useContext } from "react";
-export function Summary(){
-    const transactions = useContext(TransactionsContext);
-    
-    return (
-        <Container>
-
-
-            <div>
-                <header>
-                    <p>Entradas</p>
-                    <img src = {incomeImg} alt="Entradas"/>
-                </header>
-                <strong>R$1000,00</strong>
-            </div>
-            <div>
-                <header>
-                    <p>Saídas</p>
-                    <img src = {outcomeImg} alt="Entradas"/>
-                </header>
-                <strong>-R$1000,00</strong>
-            </div>
-            <div className="highlight-bd">
-                <header>
-                    <p>Total</p>
-                    <img src = {totalImg} alt="Entradas"/>
-                </header>
-                <strong>R$500,00</strong>
-            </div>
-        </Container>
-    );
+export function Summary() {
+  const { transactions } = useContext(TransactionsContext);
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "deposit") {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraw -= transaction.amount;
+        acc.total -= transaction.amount;
+      }
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraw: 0,
+      total: 0,
+    }
+  );
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <strong>
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(summary.deposits)}
+        </strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt="Entradas" />
+        </header>
+        <strong>
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(summary.withdraw)}
+        </strong>
+      </div>
+      <div className="highlight-bd">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Entradas" />
+        </header>
+        <strong>
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(summary.total)}
+        </strong>
+      </div>
+    </Container>
+  );
 }
